9e6e7db4f4c503151251280277bed20ed35b4d18
Tyrell Wellick <keshavniure99@gmail.com>
2026-01-03 06:12:56 -0900

    Add type definitions and functions for team and user management

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,25 @@ function getName(person: string): string {
 }
 
 console.log(getName("john Doe"));
+
+type na = {
+    name: string;
+    age?: number;
+    experience?: number;
+    isAvailable: boolean;
+}
+
+
+function makeTeam(names: na) {
+  console.log('New team registered!');
+}
+
+type newUser = {
+    first_name: string;
+    last_name: string;
+    location?: string;
+    isLogin: boolean;
+    email: string;
+    number: number;
+}
+function createNewUser(request: newUser){}
